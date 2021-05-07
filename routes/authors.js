ba31982f6b8b332b178869a01b672d606fe4c429
Prithvi-N <prithvin19@gmail.com>
2021-05-07 12:28:47 +0530

    Inital Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
-let locals = { errorMessage : `something went wrong` }
+// let locals = { errorMessage : `something went wrong` }
 
 
 // all authors
-router.get('/',(req,res)=>{
-    res.render('authors/index');
+router.get('/',async (req,res)=>{
+    let searchOptions={}
+    if(req.query.name != null && req.query.name !==''){
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try{
+        const authors= await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors: authors,
+            searchOptions: req.query
+        })
+    }catch{
+        res.redirect('/')
+    }
 });
 
 
@@ -16,23 +28,22 @@ router.get('/new',(req,res)=>{
 });
 
 // create author
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
     const author =new Author({
         name: req.body.name
     })
-    author.save((err,newAuthor)=>{
-        if (err){
-            res.render('authors/new',{
-              author:author  ,
-              locals
-            })
-        }
-        else{
-            // res.redirect(`authors/${newAuthor.id}`)
-            res.redirect(`authors`)
-        }
-    })
-    // res.send(req.body.name)
+    try{ const newAuthor= await author.save()
+         // res.redirect(`authors/${newAuthor.id}`)
+         res.redirect(`authors`)
+
+    }catch{
+        res.render('authors/new',{
+            author:author  ,
+            errorMessage : `something went wrong`
+          })
+
+    }
+  
 })
 
 module.exports = router;
