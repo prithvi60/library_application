d8552c189c5989471771efbbf4b93a6edfdfbc13
Prithvi-N <prithvin19@gmail.com>
2021-05-14 17:52:21 +0530

    Author View/Edit/Delete

--- routes/authors.js
+++ routes/authors.js
@@ -46,4 +46,22 @@ router.post('/',async (req,res)=>{
   
 })
 
+// show author
+router.get('/:id',(req,res)=> {
+    res.send('Show Author' + req.params.id)
+})
+
+// edit author
+router.get('/:id/edit',(req,res)=>{
+    res.send('Edit Author'+ req.params.id)
+})
+
+// Update Author
+router.put('/:id',(req,res)=>{
+    res.send('Update Author'+ req.params.id)
+})
+// delete author
+router.delete('/:id',(req,res)=>{
+    res.send('Delete Author'+ req.params.id)
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
   const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
+  const methodOverride = require('method-override')
   const bodyParser = require('body-parser')
-  
+ 
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
   const bookRouter = require('./routes/books')
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
+  app.use(methodOverride('_method'))
   app.use(express.static('public'))
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
